fde24536b1896114164cc13a1e2634a865fd8e8e
luizascontee <silvaconte@hotmail.com>
2022-06-06 12:21:28 +0000

    alteracoes iniciais

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html', nome='Fulano')
+  return render_template('index.html', nome='Análise de RH')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-  comentario = request.form['comentario']
-  predicao = model.predict([comentario])
+  age= request.form['age']
+  length_of_service = request.form['length_of_service']
+  awards_won = request.form['awards_won']
+  avg_training_score = request.form['avg_training_score']
+  department = request.form['department']
+  region = request.form['region']
+  comentario = request.form['previous_year_rating']
+  education = request.form['education']
+
+  predicao = model.predict([age, length_of_service, awards_won, avg_training_score, department, region, comentario, education])
   return render_template('resposta.html', predicao=predicao[0])
 
 app.run(debug=True)
